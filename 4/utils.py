0b72f73a89e3d7803bf03fae89db31f2cd7e0c55
Dafeigy <1070642765@qq.com>
2023-04-11 12:41:28 +0800

    <Update>:hw5 Finish

--- 4/utils.py
+++ 4/utils.py
@@ -1,5 +1,7 @@
 import fitz
 import cv2
+import os
+from PIL import Image
 import numpy as np
 import requests
 
@@ -7,27 +9,8 @@ import requests
 
 banner_path = 'template/banner.jpg'
 
-def pdf_to_jpg(pdf_path):
 
-    pdfdoc=fitz.open(pdf_path)
-    temp = 0
-
-    for pg in range(pdfdoc.page_count ):
-        page = pdfdoc[pg]
-        temp += 1
-        rotate = int(0)
-        # 每个尺寸的缩放系数
-        zoom_x = 0.35
-        zoom_y = 0.35
-        trans = fitz.Matrix(zoom_x, zoom_y).prerotate(rotate)
-
-        pm = page.get_pixmap(matrix=trans, alpha=False)
- 
-        pic_name = '{}.jpg'.format(temp)
-        #拼接生成pdf的文件路径
-        print(pic_name)
-
-        pm._writeIMG(f'tmp/{pic_name}',1)
+    
 
 def download_banner(id):
     img_url = f"http://api.k780.com/?app=barcode.get&bc_text={id}&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4"
@@ -35,19 +18,19 @@ def download_banner(id):
     with open('tmp/banner.jpg','wb') as f:
         f.write(req.content)
 
-def make_page():
+def make_page(page_list, output_id):
     blank = np.zeros((1042,722,3), dtype=np.uint8)
     blank[::] = 255
 
     banner = make_banner()
     blank[:86,:] = banner
     start_idx = 86
-    for i in range(4):
-        img = make_block(cv2.imread(r'tmp\1.jpg'))
+    for file_id in page_list:
+        img = make_block(cv2.imread(r'tmp\{}.jpg'.format(file_id)))
         blank[start_idx:start_idx+210,46:722-46] = img
         start_idx += 245
         
-
+    cv2.imwrite(r'output/{}.jpg'.format(output_id), blank)
     return blank
 
 def make_block(img):
@@ -73,11 +56,65 @@ def make_banner():
     return banner
 
 
+def make_paper(pdf_path):
+
+    pdfdoc=fitz.open(pdf_path)
+    temp = 0
+
+    for pg in range(pdfdoc.page_count ):
+        page = pdfdoc[pg]
+        temp += 1
+        rotate = int(0)
+        # 每个尺寸的缩放系数
+        zoom_x = 0.35
+        zoom_y = 0.35
+        trans = fitz.Matrix(zoom_x, zoom_y).prerotate(rotate)
+
+        pm = page.get_pixmap(matrix=trans, alpha=False)
+ 
+        pic_name = '{}.jpg'.format(temp)
+        #拼接生成pdf的文件路径
+        # print(pic_name)
+
+        pm._writeIMG(f'tmp/{pic_name}',1)
+
+
+    all_pages = [i for i in range(1,pdfdoc.page_count + 1)]
+    pages = [all_pages[i:i+4] for i in range(0,len(all_pages),4)]
+
+    for idx,each in enumerate(pages):
+        make_page(each, idx)
+
+def merge_pdf( folderPath, pdfFilePath ):
+    files = os.listdir( folderPath )
+    jpgFiles = []
+    sources = []
+    for file in files:
+        if 'jpg' in file:
+            jpgFiles.append( folderPath + file )
+    jpgFiles.sort()
+    output = Image.open( jpgFiles[0] )
+    jpgFiles.pop(0)
+    for file in jpgFiles:
+        jpgFile = Image.open( file )
+        if jpgFile.mode == "RGB":
+            jpgFile = jpgFile.convert( "RGB" )
+        sources.append( jpgFile )
+    output.save( pdfFilePath, "pdf", save_all=True, append_images=sources )
+
+
+
+
 
 if __name__ == "__main__":
-    img = cv2.imread(r'tmp\1.jpg')
-    page = make_page()
+    print(dir(fitz.Document))
+    in_pdf = r'demo.pdf'
+    out_pdf = r'demo-reshape.pdf'
+    make_paper(in_pdf)
+    merge_pdf('output/',out_pdf)
+
+    # page = make_page()
     
-    cv2.imshow('test',page)
-    cv2.waitKey()
+    # cv2.imshow('test',page)
+    # cv2.waitKey()
     # pdf_to_jpg(r'demo.pdf')
